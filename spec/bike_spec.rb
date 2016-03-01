65252f1738b80f3d58ac4c09e39f992f305f9e08
Gabrielle <gabivk@gmail.com>
2016-03-01 16:53:51 +0000

    Bike release test

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,5 +1,6 @@
 require 'bike'
 
-describe Bike do 
-it {is_expected.to respond_to :working?}
-end 
+describe Bike do
+  it {is_expected.to respond_to :working?}
+  it { expect(subject.working?).to be true }
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,13 @@
 require 'docking_station'
 
+
 describe DockingStation do
-  it {is_expected.to respond_to 'release_bike'}
+  it {is_expected.to respond_to :release_bike}
+  it 'expects instance of bike class' do
+    expect(subject.release_bike).to be_a Bike
+  end
+  it 'returns a true if the bike is working' do
+    bike = subject.release_bike
+  expect(bike.working?).to be true
+  end
 end
