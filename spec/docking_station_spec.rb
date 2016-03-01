f460c9c3d54695d0aa5bcf5f7b7e86b8510ce070
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-03-01 18:21:24 +0000

    added dock feature and unit test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,11 +3,21 @@ require 'docking_station'
 
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
+  it {is_expected.to respond_to :dock?}
+
   it 'expects instance of bike class' do
     expect(subject.release_bike).to be_a Bike
   end
   it 'returns a true if the bike is working' do
-    bike = subject.release_bike
-  expect(bike.working?).to be true
+    @bike = subject.release_bike
+  expect(@bike.working?).to be true
   end
+
+  it 'if a biked is docked expect true' do
+  expect(subject.dock?).to be true
+  end
+
+
+
+
 end
