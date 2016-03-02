8467f29292a4ddb07a19d2e62eb9f89974c0ae38
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-03-02 10:19:10 +0000

    added dock feature and unit test part 2

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,13 +2,26 @@ require_relative 'bike'
 
 class DockingStation
 
+  attr_reader :dock_set
+
+
+  def initialize
+    @dock_set = []
+  end
+
   def release_bike
     Bike.new
   end
 
-  def dock?
-    true
+  def dock(bike_name)
+     bike_name
   end
 
+  def dock_status
+    @dock_status.empty?
+  end
+
+  def bike
+  end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,21 +3,22 @@ require 'docking_station'
 
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
-  it {is_expected.to respond_to :dock?}
+  it {is_expected.to respond_to(:dock).with(1).argument}
+  it {is_expected.to respond_to :dock}
+  it {is_expected.to respond_to :bike}
 
   it 'expects instance of bike class' do
     expect(subject.release_bike).to be_a Bike
   end
   it 'returns a true if the bike is working' do
-    @bike = subject.release_bike
-  expect(@bike.working?).to be true
+    bike = subject.release_bike
+  expect(bike.working?).to be true
   end
 
-  it 'if a biked is docked expect true' do
-  expect(subject.dock?).to be true
+  it 'dock the bike' do
+  param = Bike.new
+  expect(subject.dock(param)).to be  param
   end
 
-
-
-
+  it 
 end
